a993b93a833ee2312b622a091b42e7815ba99e5a
RishiTiwari001 <144906747+RishiTiwari001@users.noreply.github.com>
2023-09-13 19:00:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React from "react";
 import "./App.css";
-import { useEffect } from 'react';
+import { useEffect } from "react";
 export default function IndexPage() {
-
-    console.log('heyyaaa');
-    // window.webkit.messageHandlers.bridge.postMessage("data");
+  console.log("heyyaaa");
+  // window.webkit.messageHandlers.bridge.postMessage("data");
   //  window.Android.mobileLoginForCaptchaResponse("token");
-   useEffect(() => {
-    console.log('heyyaaa');
+  useEffect(() => {
+    console.log("heyyaaa");
     // window.webkit.messageHandlers.bridge.postMessage("data");
-       window.event = new Event('event');
+    window.Event.postMessage("Hello from JS");
 
-window.addEventListener('event', function() {
-    // something is performed
-}, false);
-   window.Android.handlerFoo('event');
-       
-  window.postMessage('{"myEvent": true}');
+    window.Event = new Event("event");
 
-       window.event.postMessage('Hello from JS');
-  },[]);
- 
+    window.addEventListener(
+      "event",
+      function () {
+        // something is performed
+      },
+      true
+    );
+    window.Android.handleFoo("event");
+
+    window.postMessage('{"myEvent": true}');
+  }, []);
+
   return <div>Hello World Update</div>;
 }
