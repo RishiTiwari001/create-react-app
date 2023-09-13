87c4eb1b4ee80ad24cd5a6e5d1b0d9cff2d576c8
RishiTiwari001 <144906747+RishiTiwari001@users.noreply.github.com>
2023-09-13 18:04:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import "./App.css";
 import { useEffect } from 'react';
 export default function IndexPage() {
-  useEffect(() => {
+
     console.log('heyyaaa');
     // window.webkit.messageHandlers.bridge.postMessage("data");
   //  window.Android.mobileLoginForCaptchaResponse("token");
-    window.addEventListener("flutterInAppWebViewPlatformReady", function(event) {
-            console.log("ready");
-
-            var div = document.body.querySelector('div');
-            div.addEventListener('click', function() {
-                window.flutter_inappwebview.callHandler('handleData', 1, true, ['bar', 5], {foo: 'baz'});
-            });
-        });
+   useEffect(() => {
+    console.log('heyyaaa');
+    // window.webkit.messageHandlers.bridge.postMessage("data");
+  //  window.Android.mobileLoginForCaptchaResponse("token");
+    window.addEventListener(
+      'message',
+      function (event) {
+        console.log("event then")
+        window.flutter_inappwebview
+          .callHandler('handlerFoo', 'flutter moon')
+      }
+    );
   },[]);
+ 
   return <div>Hello World Update</div>;
 }
